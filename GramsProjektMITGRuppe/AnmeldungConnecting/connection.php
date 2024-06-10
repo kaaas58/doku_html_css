179b2ec300c7b9a4919c302554dfd949b127e84c
kaaas58 <lutr58@googlemail.com>
2024-06-10 23:47:07 +0200

    Automatisches

--- GramsProjektMITGRuppe/AnmeldungConnecting/connection.php
+++
@@ -1,8 +0,0 @@
-<?php   
-
-    $dsn = 'mysql:dbname=userdb;host=localhost';
-    $username = 'root';
-    $password = '';
-    $con = new PDO($dsn, $username, $password); // Php Data Object, dsn = Data Source Name
-
-?>
